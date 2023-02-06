89d993149121d6ff0062fb9b51de94c66e43de74
Norman Lorrain <normanlorrain@gmail.com>
2023-02-06 15:08:03 -0700

    Now creates a backup file.  If no saves, backup file is deleted.

--- pdf.py
+++ pdf.py
@@ -1,23 +1,29 @@
 import sys
 import base64
 import fitz
+import pathlib
+import os
+from backup_file import BackupFile
 
 print(fitz.__doc__)
-
-# if not tuple(map(int, fitz.VersionBind.split("."))) >= (1, 14, 5):
-#     raise SystemExit("need PyMuPDF v1.14.5 for this script")
-
-# if sys.platform == "win32":
-#     import ctypes
-
-#     ctypes.windll.shcore.SetProcessDpiAwareness(2)
 
 width, height = fitz.paper_size("letter")
 
 
 class pdf:
     def __init__(self, fname):
-        self.doc = fitz.open(fname)
+        # Be conservative and make a backup of the file first
+        self.backup = BackupFile(fname)
+
+        # Opening the PDF in memory, because during testing I found working with the
+        # file on disk would lead to occasional file corruption.
+        self.name = fname
+        inputFile = open(fname, "rb")
+        bytes = inputFile.read()
+        inputFile.close()
+
+        self.doc = fitz.Document(stream=bytes)
+
         self.page_count = len(self.doc)
         self.currentPage = 0
 
@@ -60,7 +66,23 @@ class pdf:
         new_rotation = (current_rotation + 180) % 360
         self.doc[self.currentPage].set_rotation(new_rotation)
 
+    # def saveWithBackup(self):
+    #     fpath = pathlib.Path(self.doc.name)
+    #     newstem = fpath.stem + "-"
+    #     newname = fpath.with_stem(newstem)
+
+    #     self.doc.save(
+    #         newname, incremental=False, encryption=fitz.PDF_ENCRYPT_KEEP
+    #     )  # update the file - a sub-second matter
+
+    # def saveIncremental(self):
+    #     self.doc.save(
+    #         pathlib.Path(self.doc.name),
+    #         incremental=True,
+    #         encryption=fitz.PDF_ENCRYPT_KEEP,
+    #     )  # update the file - a sub-second matter
+
     def save(self):
-        self.doc.save(
-            self.doc.name, incremental=True, encryption=fitz.PDF_ENCRYPT_KEEP
-        )  # update the file - a sub-second matter
+        # fpath = pathlib.Path(self.name)
+        self.backup.keep = True
+        self.doc.save(self.name)
