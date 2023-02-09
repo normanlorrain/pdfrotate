6b6e644b2a64006b8ee301e722fa62255fb13cfd
Norman Lorrain <normanlorrain@gmail.com>
2023-02-09 15:33:38 -0700

    WIP.  Generates MSI file, but there's ony the single .exe, nothing else.

--- wix.ps1
+++ wix.ps1
@@ -1,9 +1,16 @@
 $env:Path = 'C:\Program Files (x86)\WiX Toolset v3.11\bin;' + $env:Path
 
-heat dir dist -srd -dr INSTALLFOLDER -cg PDFrotate -gg -out dist.wxs
+# heat command from wix toolset. Scans a source for "harvesting".  Arguments:
+#    dir dist                 == specify we want to harvest a directory ("dir {source}"), source = PyInstaller's "dist" output
+#    -srd                     == suppress harvesting the root directory as an element NOT SURE IF NEEDED
+#    -cg <ComponentGroupName> 
+#    -gg                      == Generate GUIDS
+#    -out dist.wxs            == output 
+heat dir dist -srd -cg PDFrotate -gg -out dist.wxs
 
+# "Compile" the XML
 candle pdfrotate.wxs  dist.wxs
 
-
+# "Link" the XML
 light -out foobar .\pdfrotate.wixobj .\dist.wixobj
 
