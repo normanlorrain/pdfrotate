365284cb7325182e5492ed91af6aa4e6586fdeb5
Norman Lorrain <normanlorrain@gmail.com>
2023-02-09 15:03:31 -0700

    Refactor for wixobj / heat.

--- wix.ps1
+++ wix.ps1
@@ -1,9 +1,9 @@
 $env:Path = 'C:\Program Files (x86)\WiX Toolset v3.11\bin;' + $env:Path
 
-heat dir dist -srd -dr INSTALLFOLDER -cg PDFrotate -gg -out pdfrotate.wxs
+heat dir dist -srd -dr INSTALLFOLDER -cg PDFrotate -gg -out dist.wxs
 
-candle pdfrotate.wxs  pdfrotate_heat.wxs
+candle pdfrotate.wxs  dist.wxs
 
 
-light -out foobar .\pdfrotate.wixobj .\pdfrotate_heat.wixobj
+light -out foobar .\pdfrotate.wixobj .\dist.wixobj
 
