ee1f9c832a3efc59ead97883be3d90ca991129ae
Norman Lorrain <normanlorrain@gmail.com>
2023-03-02 09:22:38 -0700

    Added error checking.

--- build_wix.ps1
+++ build_wix.ps1
@@ -1,12 +1,13 @@
 $env:Path = 'C:\Program Files (x86)\WiX Toolset v3.11\bin;' + $env:Path
 
+
+
 # heat command from wix toolset. Scans a source for "harvesting".  Arguments:
 #    dir dist                 == specify we want to harvest a directory ("dir {source}"), source = PyInstaller's "dist" output
 #    -srd                     == suppress harvesting the root directory as an element NOT SURE IF NEEDED
 #    -cg <ComponentGroupName> 
 #    -gg                      == Generate GUIDS
 #    -out dist.wxs            == output 
-
 # Arguments stolen from DCSPresetManager
 # os.path.join(wix_bin, 'heat.exe'), 'dir', r'dist\DCSPresetManager',
 # '-ke', '-srd', '-v', '-ag',
@@ -14,12 +15,19 @@ $env:Path = 'C:\Program Files (x86)\WiX Toolset v3.11\bin;' + $env:Path
 # '-dr', 'RootData',
 # '-t', r'wix\appShortcut.xsl',
 # '-out', r'build\wix\src.wix'
-
 heat dir dist\PDFrotate -sw5150 -ke -srd -v -ag -cg RootDataGroup -dr RootData  -out dist.wxs
+if (-not $?) { throw "heat failed" }
+
+
+
 
 # "Compile" the XML
 candle pdfrotate.wxs  dist.wxs
+if (-not $?) { throw "candle failed" }
+
+
+
 
 # "Link" the XML
 light -out foobar -b dist\PDFrotate  .\pdfrotate.wixobj .\dist.wixobj
-
+if (-not $?) { throw "light failed" }
